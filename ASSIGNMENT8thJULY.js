2087a2853bbca237b5bd77abea96705f575e24df
DHAKSH / BUTTERSCOTCH <108130145+DHAKSH24@users.noreply.github.com>
2022-07-05 18:57:35 +0530

    Add files via upload

---
+++ ASSIGNMENT8thJULY.js
@@ -0,0 +1,16 @@
+// QUESTION : FIND IF THEY NUMBERS HAVE OPPOSITE SIGN OR NOT ! //
+
+/* ALGORITHM OF THIS PROGRAMME :
+
+1.DECLARE THE VALRIABLES AND VALUE FOR VARIABLE
+2.CHECK THE IF ELSE  CONDITION
+
+*/
+const a = 123,b = -12;
+
+  if ((a * b) < 0){
+
+    console.log("signs are  opposite");
+}
+else
+console.log("signs are not opposite");
